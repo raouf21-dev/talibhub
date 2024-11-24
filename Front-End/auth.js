52db474bed98d81172e6973a89c7037c108ec726
P-Belia <pbeliay@gmail.com>
2024-11-24 10:30:33 +0000

    removed console log v2

--- Front-End/auth.js
+++ Front-End/auth.js
@@ -142,6 +142,12 @@ async function handleSignin(event) {
     const email = document.getElementById("welcomepage-signin-email").value;
     const password = document.getElementById("welcomepage-signin-password").value;
 
+    // Debug des données envoyées
+    const requestData = {
+        email: email,
+        password: password
+    };
+
 
     try {
         const response = await fetch(`${window.location.origin}/api/auth/login`, {
