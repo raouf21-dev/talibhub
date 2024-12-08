420d7839d669767bd7996da5e3392d6d1b3cf356
P-Belia <pbeliay@gmail.com>
2024-12-08 21:16:19 +0000

    frontend url update for production & dev v2

--- Back-End/config/corsConfig.js
+++ Back-End/config/corsConfig.js
@@ -2,14 +2,14 @@
 
 const allowedOrigins = {
     development: [
+        'http://45.133.178.159',
         'http://localhost:4000',
         'http://localhost:3000',
         'http://127.0.0.1:4000',
-        'http://45.133.178.159'
     ],
     production: [
+        'http://45.133.178.159',
         process.env.FRONTEND_URL,
-        'http://45.133.178.159'
         // autres domaines de production si nécessaire
     ]
 };
