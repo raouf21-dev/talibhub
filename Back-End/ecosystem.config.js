bd5df2d7a70090b061b72e29f6b2ecfdbc76e68d
P-Belia <pbeliay@gmail.com>
2025-03-13 13:56:13 +0000

    ecosystem.config.js logs pour le débogage

--- Back-End/ecosystem.config.js
+++ Back-End/ecosystem.config.js
@@ -3,18 +3,17 @@
 module.exports = {
   apps: [
     {
-      name: 'talib-backend',
-      script: 'server.js',
-      cwd: '/home/project/TalibHub/Back-End',
+      name: "server",
+      script: "server.js",
+      cwd: "/var/www/TalibHub/Back-End",
       instances: 1,
-      exec_mode: 'fork', // Indique à PM2 d'utiliser le mode fork
+      exec_mode: "fork",
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: "1G",
       env: {
-        NODE_ENV: 'production',
-        PORT: 3000,
-        // Ajoutez d'autres variables d'environnement si nécessaire
+        NODE_ENV: "production",
+        PORT: 4000,
       },
     },
   ],
